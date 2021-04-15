2a6ed4106dd958fa32dadbad31dcac68e70f9170
awscr <awscr@protonmail.com>
2021-04-14 21:06:37 -0700

    Add 7kyu and 8kyu katas

---
+++ 8kyu/pick_a_set_of_first_elements.js
@@ -0,0 +1,22 @@
+/* https://www.codewars.com/kata/572b77262bedd351e9000076
+
+Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n 
+elements of the sequence.
+
+If n == 0 return an empty sequence []
+
+Examples:
+var arr = ['a', 'b', 'c', 'd', 'e'];
+first(arr) //=> ['a'];
+first(arr, 2) //=> ['a', 'b']
+first(arr, 3) //=> ['a', 'b', 'c'];
+first(arr, 0) //=> [];
+*/
+
+function first(arr, n = 1) {
+  return arr.slice(0, n);
+}
+
+// TESTS
+console.log(first(["a", "b", "c", "d", "e"])); //['a']
+console.log(first(["a", "b", "c", "d", "e"], 2)); //['a', 'b']
